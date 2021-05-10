92b83332926351a8bd8f0c25b78586f690baa4e4
Elsa Portilla <elsa.portilla@gmail.com>
2021-05-10 10:19:15 -0500

    webpach css import test

--- src/index.js
+++ src/index.js
@@ -5,3 +5,6 @@ import cssDesktop from "desktop.css";
 import router from '../src/routes/index';
 
 window.addEventListener('load', router);
+window.addEventListener('load', css);
+window.addEventListener('load', cssTablet);
+window.addEventListener('load', cssDesktop);
